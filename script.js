36a9f3aa09270cbece8a6232a1440d932e5d4597
Hao Lam <tuonghao2001@gmail.com>
2023-06-29 21:52:20 -0700

    prototype chain

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const hao = new Person('Hao', 2001);
 const cat = new Person('Cat', 2002);
 console.log(hao, cat);
 console.log(hao instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+hao.calcAge();
+cat.calcAge();
+console.log(hao);
+console.log(hao.__proto__);
+console.log(hao.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(hao));
+console.log(Person.prototype.isPrototypeOf(cat));
+console.log(Person.prototype.isPrototypeOf(Person));
+Person.prototype.species = 'Homo Sapiens';
+console.log(hao.species, cat.species);
+console.log(hao.hasOwnProperty('birthYear'));
+console.log(hao.hasOwnProperty('species'));
