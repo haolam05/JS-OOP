2b8b11a9eccc9c1c25cb293edf10382f8a696c3b
Hao Lam <tuonghao2001@gmail.com>
2023-06-30 02:10:07 -0700

    Coding Challenge #3

--- script.js
+++ script.js
@@ -217,3 +217,32 @@ den.introduce();
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
+
+//////////////////////////////////////////////////
+// Coding Challenge #3
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.constructor = EV;
+EV.prototype.chargeBattery = function (chargTo) {
+  this.charge = chargTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+const tesla = new EV('Tesla', 120, 23);
+console.dir(tesla);
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery(90);
+tesla.accelerate();
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
