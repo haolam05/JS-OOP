c01c841aa7c5aabc9708d47c2876130ae1419eed
Hao Lam <tuonghao2001@gmail.com>
2023-06-29 21:14:54 -0700

    constructors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// constructor function
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+const hao = new Person('Hao', 2001);
+const cat = new Person('Cat', 2002);
+console.log(hao, cat);
+console.log(hao instanceof Person);
